0f395af02673a77b577d1c3ad0398b921e2562a6
Daniela <daniela.vornic@gmail.com>
2020-08-15 14:05:39 +0300

    create tweet function

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import tweepy
+import tweepy, requests, os
 
 consumer_key = 'lnYxJ2kGKKWQnKUyn4omuNCLj' 
 consumer_secret = 'fRDKspUvcUQEklWhCXeZJi2Et58FwDhtOV71fTNIr5Pc1XtIy0' 
@@ -7,3 +7,19 @@ access_token_secret = 'NZU2wmq3Uz0egyu1xKH92eIFbpoPUNZV2VB53dRWjYBU8'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+
+def tweet():
+    filename = 'cat.jpg'
+    r = requests.get('https://api.thecatapi.com/v1/images/search')
+    url = r.json()[0]['url']
+    request = requests.get(url, stream=True)
+    if request.status_code == 200:
+        with open(filename, 'wb') as image:
+            for chunk in request:
+                image.write(chunk)
+        api.update_with_media(filename)
+        os.remove(filename)
+    else:
+        tweet()
+
+tweet()
