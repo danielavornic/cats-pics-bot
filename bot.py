116d3f9fc3db8af5990bdf28092bc64eb58a9592
Daniela <daniela.vornic@gmail.com>
2020-08-15 15:54:33 +0300

    store twitter keys as environment variables

--- bot.py
+++ bot.py
@@ -3,10 +3,10 @@ import requests
 import os
 from time import sleep
 
-consumer_key = 'lnYxJ2kGKKWQnKUyn4omuNCLj' 
-consumer_secret = 'fRDKspUvcUQEklWhCXeZJi2Et58FwDhtOV71fTNIr5Pc1XtIy0' 
-access_token = '1285590029776097280-xRUcCQ4nEEiYYTG6Vj4be1FcinnKR3' 
-access_token_secret = 'NZU2wmq3Uz0egyu1xKH92eIFbpoPUNZV2VB53dRWjYBU8' 
+consumer_key = os.environ['CONSUMER_KEY']
+consumer_secret = os.environ['CONSUMER_SECRET']
+access_token = os.environ['ACCESS_KEY']
+access_token_secret = os.environ['ACCESS_SECRET']
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
