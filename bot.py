bebb5c9400b23769cc53ca26a6528a921423edff
Daniela <daniela.vornic@gmail.com>
2020-08-15 12:47:31 +0300

    login into account

---
+++ bot.py
@@ -0,0 +1,9 @@
+import tweepy
+
+consumer_key = 'lnYxJ2kGKKWQnKUyn4omuNCLj' 
+consumer_secret = 'fRDKspUvcUQEklWhCXeZJi2Et58FwDhtOV71fTNIr5Pc1XtIy0' 
+access_token = '1285590029776097280-xRUcCQ4nEEiYYTG6Vj4be1FcinnKR3' 
+access_token_secret = 'NZU2wmq3Uz0egyu1xKH92eIFbpoPUNZV2VB53dRWjYBU8' 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
