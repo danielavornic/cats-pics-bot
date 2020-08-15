1b3888a51162e557790dcfd58fae8234d3de25a6
Daniela <daniela.vornic@gmail.com>
2020-08-15 16:10:50 +0300

    delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-t.status_code == 200:
-        with open(filename, 'wb') as image:
-            for chunk in request:
-                image.write(chunk)
-        api.update_with_media(filename)
-        os.remove(filename)
-    else:
-        tweet()
-    sleep(120)
